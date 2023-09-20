bbff5631814ed8397d2bd459a9a5349eb85cde09
AppiahEnoch <prignutt@gmail.com>
2023-09-20 03:34:16 -0700

    wwwww

--- loadCards.js
+++ loadCards.js
@@ -1,3 +1,13 @@
+
+$(document).ready(function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
+
+
+
 function generateCards() {
     $.ajax({
       type: "post",
