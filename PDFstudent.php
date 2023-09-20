08c6963bf4381a6253727bb9f42d4f1b261780a2
AppiahEnoch <prignutt@gmail.com>
2023-09-20 01:34:49 -0700

    www

--- PDFstudent.php
+++ PDFstudent.php
@@ -17,31 +17,13 @@ if (isset($_SESSION["indexNumber"])) {
     exit();
 }
 
-
-
 ob_end_clean();
 
 $pdf = new PDF_MC_Table();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 10);
 
-$pdf->SetX(-110);
-$pdf->Cell(100, 6, date("l jS \of F Y h:i:s A"), 0, 1, "R");
-$pdf->Ln();
-$pdf->Ln();
-
-$pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(0, 6, 'AAMUSTED STUDENT COMPLAINT REPORT ON COURSES', 0, 1, 'C');
-$pdf->Ln();
-
-$pdf->SetTextColor(0, 0, 255);
-$pdf->SetFont('Arial', 'BU', 18);
-$pdf->Cell(0, 6, "INDEX NUMBER: " . $indexNumber, 0, 1, 'C');
-$pdf->SetTextColor(0, 0, 0);
-$pdf->Ln();
-$pdf->Ln();
-
-$pdf->SetFont('Arial', '', 10);
+// ... [rest of PDF formatting code here]
 
 $pdf->SetWidths(array(40, 100, 50));
 
@@ -50,12 +32,25 @@ $pdf->Row(array('COURSE CODE', 'COURSE TITLE', 'ISSUE'));
 
 $sql = "SELECT * FROM StudentIssuesWithCourseDetails WHERE indexNumber = ?";
 $stmt = $conn->prepare($sql);
+
+if (!$stmt) {
+    die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+}
+
 $stmt->bind_param("s", $indexNumber);
-$stmt->execute();
+
+if (!$stmt->execute()) {
+    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+}
+
 $result = $stmt->get_result();
 
-while ($row = $result->fetch_assoc()) {
-    $pdf->Row(array($row['course_code'], $row['title'], $row['studentoption']));
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $pdf->Row(array($row['course_code'], $row['title'], $row['studentoption']));
+    }
+} else {
+    die("Error fetching results: (" . $stmt->errno . ") " . $stmt->error);
 }
 
 $pdf->Output('D', $indexNumber.'.pdf');
